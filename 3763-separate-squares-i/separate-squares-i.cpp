2055219eeb1693a9fb7df61433a75b481ab06904
Nidhi Chauhan <nidhichauhan2460@gmail.com>
2026-01-14 01:08:21 +0530

    Time: 323 ms (5.23%) | Memory: 198.6 MB (73.55%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -2,39 +2,31 @@ class Solution {
 public:
     double separateSquares(vector<vector<int>>& squares) {
         double low = 1e18, high = 0;
+
         for (auto &sq : squares) {
             low = min(low, (double)sq[1]);
             high = max(high, (double)sq[1] + sq[2]);
         }
 
-        auto areaAbove = [&](double yLine) {
-            double area = 0;
+        auto areaDiff = [&](double y) {
+            double diff = 0;
             for (auto &sq : squares) {
-                double bottom = sq[1], top = sq[1] + sq[2];
-                if (top <= yLine) continue; 
-                double height = top - max(bottom, yLine);
-                area += height * sq[2];
+                double bottom = sq[1];
+                double top = sq[1] + sq[2];
+                double side = sq[2];
+                if (top > y)
+                    diff += max(0.0, top - max(bottom, y)) * side;
+                if (bottom < y)
+                    diff -= max(0.0, min(top, y) - bottom) * side;
             }
-            return area;
+            return diff;
         };
-
-        auto areaBelow = [&](double yLine) {
-            double area = 0;
-            for (auto &sq : squares) {
-                double bottom = sq[1], top = sq[1] + sq[2];
-                if (bottom >= yLine) continue; 
-                double height = min(top, yLine) - bottom;
-                area += height * sq[2];
-            }
-            return area;
-        };
-
         for (int i = 0; i < 100; i++) {
             double mid = (low + high) / 2.0;
-            double above = areaAbove(mid);
-            double below = areaBelow(mid);
-            if (above > below) low = mid;
-            else high = mid;
+            if (areaDiff(mid) > 0)
+                low = mid;
+            else
+                high = mid;
         }
         return low;
     }
