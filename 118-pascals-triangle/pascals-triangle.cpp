bc02fd748a56032e2c7eb2a621375e0fc1c8fad4
Nidhi Chauhan <nidhichauhan2460@gmail.com>
2026-01-15 00:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (63.14%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> triangle(numRows);
         
         for (int i = 0; i < numRows; i++) {
-            triangle[i].resize(i + 1, 1); // first & last = 1
+            triangle[i].resize(i + 1, 1); 
             for (int j = 1; j < i; j++) {
                 triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
             }
