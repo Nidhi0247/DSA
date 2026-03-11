b909a426e7908822b5ef32858c7ae1d1e19af3ae
Nidhi Chauhan <nidhichauhan2460@gmail.com>
2026-03-11 22:50:20 +0530

    Time: 8 ms (47.03%) | Memory: 25.9 MB (10.19%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = nums[i];
+            } else {
+                int steps = nums[i];
+                int newIndex = ((i + steps) % n + n) % n; 
+                result[i] = nums[newIndex];
+            }
+        }
+        return result;
+    }
+};
