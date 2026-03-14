1b06aa5b0f7260b34acacda7856bf2d9a72b572c
Nidhi Chauhan <nidhichauhan2460@gmail.com>
2026-03-14 22:49:09 +0530

    Time: 3 ms (59.10%) | Memory: 8 MB (80.34%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void generateHappyStrings(string &current, vector<string> &result, int n) {
-        if (current.size() == n) {
-            result.push_back(current);
-            return;
-        }
-
-        for (char ch : {'a', 'b', 'c'}) {
-            if (current.empty() || current.back() != ch) {
-                current.push_back(ch);
-                generateHappyStrings(current, result, n);
-                current.pop_back();
-            }
-        }
-    }
-
     string getHappyString(int n, int k) {
-        vector<string> result;
-        string current = "";
-        generateHappyStrings(current, result, n);
-        return k <= result.size() ? result[k - 1] : "";
+        char dict[3] = {'a', 'b', 'c'};
+        int cnt = 0;
+        string ans = "";
+        auto dfs = [&] (this auto&& dfs, int i, string& tmp) {
+            if(i == n)  {
+                cnt += 1;
+                if(cnt == k) ans = tmp;
+                return;
+            }
+            for(char c : dict) {
+                if(cnt == k) break;
+                if(tmp.empty() || tmp.back() != c) {
+                    tmp.push_back(c);
+                    dfs(i + 1, tmp);
+                    tmp.pop_back();
+                }
+            }
+        };
+        string tmp = "";
+        dfs(0, tmp);
+        return ans;
     }
 };
