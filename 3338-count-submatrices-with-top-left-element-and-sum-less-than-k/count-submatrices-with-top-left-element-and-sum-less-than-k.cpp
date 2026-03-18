1951718ac04299a4ad1fc8bf57baaf5648d33c19
Nidhi Chauhan <nidhichauhan2460@gmail.com>
2026-03-18 21:39:48 +0530

    Time: 4 ms (98.88%) | Memory: 90.4 MB (82.02%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> prefix(m, vector<int>(n, 0));
-        int count = 0;
+    int countSubmatrices(vector<vector<int>>& grid, int k) 
+    {
+        int n=grid.size(),m=grid[0].size();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                prefix[i][j] = grid[i][j];
-                if (i > 0) prefix[i][j] += prefix[i-1][j];
-                if (j > 0) prefix[i][j] += prefix[i][j-1];
-                if (i > 0 && j > 0) prefix[i][j] -= prefix[i-1][j-1];
+        for(int i=1; i<m; i++)
+        {
+            grid[0][i] += grid[0][i-1];
+        }
 
-                if (prefix[i][j] <= k) count++;
+        for(int i=1; i<n; i++)
+        {
+            int prefix=grid[i][0];
+            grid[i][0] = grid[i-1][0] + prefix;
+            for(int j=1; j<m; j++)
+            {
+                prefix+=grid[i][j];
+                grid[i][j] = grid[i-1][j]+prefix;
             }
         }
-        return count;
+
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] <= k) ans++;
+            }
+        }
+        return ans;
+        
     }
 };
