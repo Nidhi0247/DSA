6d8a6d173747f5de56ed5c42d76c43619f3e8736
Nidhi Chauhan <nidhichauhan2460@gmail.com>
2026-05-24 05:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (24.73%) - LeetSync

--- 2083-three-divisors/three-divisors.cpp
+++ 2083-three-divisors/three-divisors.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int count =0;
-        for(int i=1;i<=n;i++ )
-        if(n%i==0) count++;
-        if(count==3) return true;
-        else return false;
+        int root = sqrt(n);
+        if(root*root!=n) return false;
+        for(int i=2 ;i*i<=root;i++){
+            if(root%i==0)return false;
+        }
+        return root>1;
     }
 };
